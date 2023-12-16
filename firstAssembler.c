a79b2205987fd4f45d48fc3b9e872cdfa6decd7c
matansegev <134601287+matansegev@users.noreply.github.com>
2023-12-16 19:00:14 +0200

    pre + first

---
+++ firstAssembler.c
@@ -0,0 +1,78 @@
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+// #include <ctype.h>
+// #include "structures.h"
+// #include "registerTable.h"
+// #include "firstAssembler.h"
+// #include "secondAssembler.h"
+// #include "errors.h"
+
+// void fPass(char *fileName)
+// {}
+
+// void emptyIntArray(int array[], int n) /* set array to empty */
+// {}
+
+// void nextExpression(char **ptr, int *count, int *wordLength, char **word)
+// {}
+
+// void ignoreSpaceTab(char **ptr)
+// {}
+
+// bool validRow(char *ptr, int lineNum)
+// {}
+
+// bool labelCheck(char *ptr, char *word, int count, int wordLength, gNode *hSymbol, gNode temp, int lineNum)
+// {}
+
+// bool validLabel(gNode *list, char *ptr, int lineNum)
+// {}
+
+// bool isEntryExtern(char *ptr)
+// {}
+
+// bool isData(char *ptr)
+// {}
+
+// bool isString(char *ptr)
+// {}
+
+// int isCommand(char *ptr)
+// {}
+
+// int analyzeData(char *ptr, gNode *hRow, int lineNum)
+// {}
+
+// int analyzeString(char *ptr, gNode *hRow, int lineNum)
+// {}
+
+// int analyzeOperands(bool special, char *ptr, int commandIndex, int *operandType, gNode *hSuspectLabel, gNode *hRow, int lineNum, int IC)
+// {}
+
+// int immidiateCheck(char *word, gNode row, int opNum, int lineNum)
+// {}
+
+// bool isRegister(char *name, gNode row, int index)
+// {}
+
+// bool opernadsTypeCheck(gNode row)
+// {}
+
+// bool checkValidInstrucion(char *ptr, int opNum, int commandIndex, int *operandType, int lineNum)
+// {}
+
+// bool isSigns(char *ptr)
+// {}
+
+// int numCheck(char *ptr, gNode row, int opNum)
+// {}
+
+// int wordLen(char *ptr)
+// {}
+
+// int endOfLine(char *ptr)
+// {}
+
+// void addICtoDataAddress(gNode *HEAD, int IC)
+// {}
--- preAssembler.c
+++ preAssembler.c
@@ -1,6 +1,8 @@
 #include "preAssembler.h"
 #include "genericH.h"
 
+void extractMcr(node, char **, FILE **);
+
 void fileCompiling(char* fileName) {
     // Create the output file name
     char outputFileName[100];
@@ -22,9 +24,10 @@ void fileCompiling(char* fileName) {
         fclose(inputFile);
         return;
     }
-
+    extractMcr(*inputFile, *outputFile);
+)
     // Read line by line from the input file
-    char line[1000];//what is the for each line?
+    char line[MAX_LINE_SIZE];//what is the for each line?
     while (fgets(line, sizeof(line), inputFile)) {
         // Append "**" to the line
         strcat(line, "**!");
@@ -37,4 +40,76 @@ void fileCompiling(char* fileName) {
     fclose(inputFile);
     fclose(outputFile);
 }
+void openFiles(char **fileName, FILE **originalFile, FILE **modifiedFile)
+{}
+
+/** extractMcr
+ * writting the macro that declared to certain place inside am file
+ * @param node - node of certain macro - the macros are stored inside linked list
+ * @param fileName - the name of file as written in terminal
+ * @param modified - the modified file with .am extenssion after extracting the macros
+ * */
+void extractMcr(FILE *inputFile, FILE *outputFile)
+{
+    char line[MAX_LINE_SIZE];//what is the for each line?
+    while (fgets(line, sizeof(line), inputFile)) {
+        // Append "**" to the line
+        strcat(line, "**!");
+
+        // Write the modified line to the output file
+        fputs(line, outputFile);
+    }
+    
+    
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     int index = 1;
+// 	int start, end;
+//     char *ptr;
+// 	char filePath[MAXIMUM_FILE_NAME];
+//     char line[LINE_LEN];
+// 	FILE *original;
+// 	sprintf(filePath,"%s.as",*(fileName));
+// 	original = fopen(filePath, "r");
+// 	start = (int)getStartMacro(node);
+// 	end = (int)getEndMacro(node);
+
+//     while (fgets(line, sizeof(line), original) != NULL)
+// 	{
+// 		if (index < start)
+// 		{
+// 			index += 1;
+// 		}
+// 		else
+// 		{
+// 			if (start <= end)
+// 			{
+// 				ptr = (char *)&line;
+// 				ignoreSpaceTab(&ptr);
+// 				fputs(ptr, *modified);
+// 				start += 1;
+// 				i = start;
+// 			}
+// 			else
+// 				break;
+// 		}
+// 	}
+// 	fclose(original);
+// }
+
+// int checkMcrFun(char *beginLine, bool *isMCRO)
+// {}
     
