d226978724e50d11b0a61f951a13a02410f7ed30
EinavFerrera <134479183+EinavFerrera@users.noreply.github.com>
2023-12-16 16:56:52 +0200

    file creation

---
+++ main.c
@@ -0,0 +1,19 @@
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("Error - file name is required!\n");
+		return 0;
+	}
+
+	/*compiling each file*/
+	else
+	{
+		int i;
+		for (i = 1; i < argc; i++)
+		{
+			fileCompiling(argv[i]);
+		}
+	}
+	return 0;
+}
